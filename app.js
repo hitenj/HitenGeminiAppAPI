ae335b8f90796bd743825ab8945d32d172496e10
Hiten <hgeetaj@gmail.com>
2025-02-03 01:26:17 +0530

    Demo

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const cors = require('cors')
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/getResponse', (req, res) => {
+    console.log(req.body.question)
+
+    const genAI = new GoogleGenerativeAI('AIzaSyBii1b8w2aGwnTRpLo2sHPDgFqTTxpD7Wg');
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    model.generateContent(req.body.question).then(result=>{
+        console.log(result.response.text());
+        const response = result.response.text();
+        res.status(200).json({
+            response: response
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err 
+        })
+    }) 
+})
+
+app.get('*', (req, res)=>{
+    res.status(404).json({
+        msg:'bad request'
+    })
+})
+
+module.exports = app;
